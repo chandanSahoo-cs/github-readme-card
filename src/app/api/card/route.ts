d0c31c6ca11320b790a2d5177b89fa79fa8b2d64
Chandan <chandansahoo02468@gmail.com>
2025-08-28 23:45:06 +0530

    Add server and client side caching

--- src/app/api/card/route.ts
+++ src/app/api/card/route.ts
@@ -1,9 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // app/api/card/route.ts
 import { profileSVG } from "@/app/lib/svg";
+let latestSVG: string | null = null;
+let latestSVGTime = 0;
+const BUFFER_TIME = 1000 * 60 * 60;
+
 export async function GET() {
-  const svg = await profileSVG();
-  return new Response(svg, {
+  const now = Date.now();
+
+  if (!latestSVG || now - latestSVGTime > BUFFER_TIME) {
+    latestSVG = await profileSVG();
+    latestSVGTime = now;
+  }
+
+  return new Response(latestSVG, {
     headers: {
       "Content-Type": "image/svg+xml",
       "Cache-Control": "public, max-age=3600, stale-while-revalidate=60",
