78a30282ed27f44a14efe09d51150dd02f2b146a
Chandan <chandansahoo02468@gmail.com>
2025-08-28 23:23:35 +0530

    Add cache-control

--- src/app/api/card/route.ts
+++ src/app/api/card/route.ts
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // app/api/card/route.ts
 import { profileSVG } from "@/app/lib/svg";
-
 export async function GET() {
   const svg = await profileSVG();
   return new Response(svg, {
-    headers: { "Content-Type": "image/svg+xml" },
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": "public, max-age=3600, stale-while-revalidate=60",
+    },
   });
 }
 
