02d722c4d7eec5a904d69acf6fd10fa164d791de
chandan <chandansahoo02468@gmail.com>
2025-11-03 20:50:03 +0530

    Change cache time

--- src/app/api/card/route.ts
+++ src/app/api/card/route.ts
@@ -1,33 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // app/api/card/route.ts
-import { fetchedData, profileSVG, type UserProfile } from "@/app/lib/svg";
-let currentSVG: string | null = null;
-let currentSVGTime = 0;
-
-let currentProfileData: UserProfile | null = null;
-
-const BUFFER_TIME = 1000*60*60;
-const INTERVAL_TIME = 1000;
-
-setInterval(async () => {
-  const fetchProfileData = await fetchedData();
-  if (fetchProfileData !== currentProfileData) {
-    currentProfileData = fetchProfileData;
-  }
-}, INTERVAL_TIME);
+import { profileSVG } from "@/app/lib/svg";
 
 export async function GET() {
-  const now = Date.now();
-  const fetchProfileData = await fetchedData();
-  if (!fetchProfileData || !currentSVG || now - currentSVGTime > BUFFER_TIME) {
-    currentSVG = await profileSVG();
-    currentSVGTime = now;
-  }
+  const currentSVG = await profileSVG();
 
   return new Response(currentSVG, {
     headers: {
       "Content-Type": "image/svg+xml",
-      "Cache-Control": "public, max-age=10, stale-while-revalidate=60",
+      "Cache-Control": "public, max-age=1000, stale-while-revalidate=60",
     },
   });
 }
