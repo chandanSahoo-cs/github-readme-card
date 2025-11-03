0315dff790fe6746600aebea7be027c01dd27b34
chandan <chandansahoo02468@gmail.com>
2025-11-03 19:48:31 +0530

    Test workflow

--- src/app/lib/svg.ts
+++ src/app/lib/svg.ts
@@ -420,7 +420,7 @@ const profileSVG = async () => {
         </style>
         <rect width="100%" height="100%" fill="#0d1117" />
         <text x="20" y="40" class="prompt">$ git connect https://github.com/chandanSahoo-cs</text>
-        <text x="20" y="70" class="text">Failed to connect (>_<)</text>
+        <text x="20" y="70" class="text">Failed to connect ${error}</text>
         <text x="20" y="90" class="text">Check out my projects until then :)</text>
         </svg>
         `;
